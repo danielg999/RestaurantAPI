3d9015847be28ca81b7ba3b665f3b1af064f77c5
danielg999 <daniel.gralik5@gmail.com>
2022-01-06 17:02:09 +0100

    Modified web api requests

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restaurants = _dbContext
+                .Restaurants
+                .ToList();
+            return Ok(restaurants);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute]int id)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .FirstOrDefault(r => r.Id == id);
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(restaurant);
+            }
+            
+        }
+    }
+}
