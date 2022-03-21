2803b926f3dc5a2c05b8e84fc49e54ccbaec226f
danielg999 <daniel.gralik5@gmail.com>
2022-03-21 15:50:47 +0100

    Add validation to pagination query

--- Startup.cs
+++ Startup.cs
@@ -77,6 +77,7 @@ namespace RestaurantAPI
             services.AddScoped<IAccountService, AccountService>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+            services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValitator>();
             services.AddScoped<ErrorHandlingMiddleware>();
             services.AddScoped<RequestTimeMiddleware>();
             services.AddScoped<IUserContextService, UserContextService>();
---
+++ Validators/RestaurantQueryValitator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RestaurantAPI.Models;
+using System.Linq;
+
+namespace RestaurantAPI.Validators
+{
+    public class RestaurantQueryValitator : AbstractValidator<RestaurantQuery>
+    {
+        private int[] allowedPageSizes = new[] { 5, 10, 15 };
+        public RestaurantQueryValitator()
+        {
+            RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+            RuleFor(r => r.PageSize).Custom((value, context) =>
+            {
+                if (!allowedPageSizes.Contains(value))
+                {
+                    context.AddFailure("PageSize", $"PageSize must in [{string.Join(",", allowedPageSizes)}]");
+                }
+            });
+        }
+    }
+}
